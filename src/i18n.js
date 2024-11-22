09912eead6c544888a050c0fdf26d9815a40f832
ozmberkan <ozmberkan@gmail.com>
2024-11-22 10:19:45 +0300

    Hero changed

--- src/i18n.js
+++ src/i18n.js
@@ -5,7 +5,7 @@ import LanguageDetector from "i18next-browser-languagedetector";
 const resources = {
   en: {
     translation: {
-      heroTitle: "Front End Developer / React Developer",
+      heroTitle: "Front End Developer React Developer",
       discoverText: "Discover my projects.",
       discoverButton: "Discover",
       about: `As a Front End developer & designer, we bring digital projects to
@@ -24,7 +24,7 @@ const resources = {
   },
   tr: {
     translation: {
-      heroTitle: "Front End Geliştiricisi / React Geliştiricisi",
+      heroTitle: "Front End Geliştiricisi React Geliştiricisi",
       discoverText: "Projelerimi keşfedin.",
       discoverButton: "Keşfet",
       about: `Ben bir Ön Uç Geliştirici & Tasarımcı olarak dijital projelere hayat veriyorum. 2024 yılında Dokuz Eylül Üniversitesi ‘Çalışma Ekonomisi ve Endüstri İlişkileri’ bölümünden mezun oldum ve 1.5 yıldır yazılım dünyasında aktif olarak yer alıyorum. İzmir’de yaşıyorum ve her geçen gün kendimi daha da geliştirerek React, TailwindCSS, Figma, Photoshop gibi teknolojilerde yetkinleşiyorum. Pixel-perfect tasarım anlayışım ile çalışmayı seviyor, hem kodlama hem de tasarım konusunda detaylara büyük önem veriyorum. Takım çalışmasına yatkınım ve güçlü iletişim becerilerimle katkıda bulunuyorum. React konusunda derinleşerek uzmanlaşma yolunda ilerliyorum. Henüz bir yazılım sektöründe profesyonel deneyimim olmasa da, sürekli kendimi eğiterek projeler geliştiriyorum ve bu projeleri düzenli olarak güncelliyorum.`,
